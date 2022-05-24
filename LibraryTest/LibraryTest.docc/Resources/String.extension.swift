1f967f0a704ab22940ddd43d1afe172402e21448
sayyajp <y.m.i.m@icloud.com>
2022-05-25 00:27:10 +0900

    Add extension

---
+++ LibraryTest/LibraryTest.docc/Resources/String.extension.swift
@@ -0,0 +1,15 @@
+//
+//  String.extension.swift
+//  
+//
+//  Created by seiya niimi on 2022/05/25.
+//
+
+import UIKit
+
+public extension String {
+
+    public func echo() {
+        print(self)
+    }
+}
