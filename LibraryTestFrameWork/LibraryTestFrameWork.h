08acefb38a3cea7e0ce2969776e919ef37fead85
sayyajp <y.m.i.m@icloud.com>
2022-05-25 00:48:09 +0900

    modify podspec

---
+++ LibraryTestFrameWork/LibraryTestFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  LibraryTestFrameWork.h
+//  LibraryTestFrameWork
+//
+//  Created by seiya niimi on 2022/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibraryTestFrameWork.
+FOUNDATION_EXPORT double LibraryTestFrameWorkVersionNumber;
+
+//! Project version string for LibraryTestFrameWork.
+FOUNDATION_EXPORT const unsigned char LibraryTestFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryTestFrameWork/PublicHeader.h>
+
+
