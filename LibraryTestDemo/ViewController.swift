a4bd6dce926a39908b32c9465e8090d55ce616e5
sayyajp <y.m.i.m@icloud.com>
2022-05-24 23:58:51 +0900

    Initial Commit

---
+++ LibraryTestDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LibraryTestDemo
+//
+//  Created by seiya niimi on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
