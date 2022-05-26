2c82ec950da14e92c812494c50e1e48e8bcd642e
sayyajp <y.m.i.m@icloud.com>
2022-05-26 17:45:50 +0900

    Add xcframework and modify podspec

---
+++ build/Sample-iOS-simulator.xcarchive/Products/Library/Frameworks/LibraryTest.framework/Headers/LibraryTest.h
@@ -0,0 +1,18 @@
+//
+//  LibraryTest.h
+//  LibraryTest
+//
+//  Created by seiya niimi on 2022/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LibraryTest.
+FOUNDATION_EXPORT double LibraryTestVersionNumber;
+
+//! Project version string for LibraryTest.
+FOUNDATION_EXPORT const unsigned char LibraryTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibraryTest/PublicHeader.h>
+
+
